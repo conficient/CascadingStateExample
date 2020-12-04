a0e54d9f2b71e6875db6a0fd03e8999d4dfce803
Howard Richards <dev@conficient.com>
2020-12-04 17:57:43 +0000

    Add project files.

---
+++ CascadingStateExample/States/ButtonStates.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace CascadingStateExample.States
+{
+    public class ButtonStates
+    {
+        /// <summary>
+        /// Button visibility 
+        /// </summary>
+        public bool Visible
+        {
+            get => _visible; 
+            set
+            {
+                // only trigger if value changes
+                if(value != _visible)
+                {
+                    // change visibility and raise event
+                    _visible = value;
+                    OnVisibilityChanged?.Invoke(_visible);
+                }
+            }
+        }
+
+        /// <summary>
+        /// field
+        /// </summary>
+        private bool _visible = false;
+
+        /// <summary>
+        /// Event raised when visibility changes - this allows components to rerender via StateHasChanged()
+        /// </summary>
+        public event Action<bool> OnVisibilityChanged;
+
+        /// <summary>
+        /// Event raised when Save button is clicked
+        /// </summary>
+        public event Action<string> OnSaved;
+
+        /// <summary>
+        /// Event raised when Cancel button is clicked
+        /// </summary>
+        public event Action<string> OnCancelled;
+
+        /// <summary>
+        /// Raise save event
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Save(string msg) => OnSaved?.Invoke(msg);
+        
+        /// <summary>
+        /// Raise cancel event
+        /// </summary>
+        /// <param name="msg"></param>
+        public void Cancel(string msg) => OnSaved?.Invoke(msg);
+
+    }
+}
